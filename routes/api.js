daca275ac60c50cbba4e4ace8d76369471478cbb
William Velarde <williamvelarde@Williams-MacBook-Air.local>
2017-04-11 12:11:07 -0600

    Deploy

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var Status = require('../models/status')
+
+Status.methods(['get', 'put', 'post', 'delete'])
+Status.register(router, '/status');
+
 router.get('/status', function(req, res){
-    res.send('api is working');
+    res.send('');
 });
 
 module.exports = router;
